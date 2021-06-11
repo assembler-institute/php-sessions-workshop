756d9b1c4668a68e764c88ec3e39c80c100d36a5
manuelsanchosanchez <manuel.sancho.sanchez@gmail.com>
2021-06-11 10:33:58 +0200

    Highlighted username in dashboard

--- workshop/demo/dashboard.php
+++ workshop/demo/dashboard.php
@@ -142,7 +142,7 @@ if (!isset($_SESSION["email"])) {
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Welcome <?= strstr($_SESSION["email"], '@', true) ?>, this is our Dashboard</h1>
+          <h1 class="h2">Welcome <span class="text-primary"><?= strstr($_SESSION["email"], '@', true) ?></span>, this is our Dashboard</h1>
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
