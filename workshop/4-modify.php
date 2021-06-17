a19d1c6dd2b124ae12d5ce8bc878dd1de976060d
manuelsanchosanchez <manuel.sancho.sanchez@gmail.com>
2021-06-17 15:08:48 +0200

    updated workshop

--- workshop/4-modify.php
+++ workshop/4-modify.php
@@ -1,16 +1,8 @@
-<div>
-    <div class="bd-example">
-        <div class="accordion" id="modifyAccordion">
-            <div class="accordion-item">
-                <h4 class="accordion-header" id="modifyHeading">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseModify" aria-expanded="false" aria-controls="collapseModify">
-                        Modifying session variables
-                    </button>
-                </h4>
-                <div id="collapseModify" class="accordion-collapse collapse" aria-labelledby="modifyHeading">
-                    <div class="accordion-body">
-                        <p>We can modify our session variables just assigning them different values.</p>
-                        <pre><code>&#60?
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Modifying session variables</h5>
+        <p>We can modify our session variables just assigning them different values.</p>
+        <pre><code>&#60?
 
 // Start session
 session_start();
@@ -22,13 +14,9 @@ $_SESSION["username"] = "haterman";
 unset($_SESSION["email"]);
 
 ?></code></pre>
-                        <div class="alert alert-info" role="alert">
-                            <h6 class="alert-heading">Deleting a session variable</h6>
-                            <p class="mb-0">We can delete a session variable with <code>unset()</code></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="alert alert-info" role="alert">
+            <h6 class="alert-heading">Deleting a session variable</h6>
+            <p class="mb-0">We can delete a session variable with <code>unset()</code></p>
         </div>
     </div>
 </div>
--- workshop/5-destroy.php
+++ workshop/5-destroy.php
@@ -1,16 +1,8 @@
-<div>
-    <div class="bd-example">
-        <div class="accordion" id="destroyAccordion">
-            <div class="accordion-item">
-                <h4 class="accordion-header" id="destroyHeading">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDestroy" aria-expanded="false" aria-controls="collapseDestroy">
-                        Destroying session variables
-                    </button>
-                </h4>
-                <div id="collapseDestroy" class="accordion-collapse collapse" aria-labelledby="destroyHeading">
-                    <div class="accordion-body">
-                        <p>If we want to completely destroy the session first we must use <code>unset($_SESSION)</code> to delete all the session variables, then we must delete the cookie in case we use cookies for session id spreading and finaly we can use <code>session_destroy()</code>.</p>
-                        <pre><code>&#60?
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Destroying session variables</h5>
+        <p>If we want to completely destroy the session first we must use <code>unset($_SESSION)</code> to delete all the session variables, then we must delete the cookie in case we use cookies for session id spreading and finaly we can use <code>session_destroy()</code>.</p>
+        <pre><code>&#60?
 
 // Start session
 session_start();
@@ -36,13 +28,9 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 ?></code></pre>
-                        <div class="alert alert-info" role="alert">
-                            <h6 class="alert-heading">Deleting a session variable</h6>
-                            <p class="mb-0">We can delete a session variable with <code>unset()</code></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="alert alert-info" role="alert">
+            <h6 class="alert-heading">Deleting a session variable</h6>
+            <p class="mb-0">We can delete a session variable with <code>unset()</code></p>
         </div>
     </div>
 </div>
