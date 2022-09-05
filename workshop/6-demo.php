14a0d957c7e35c3a520caeabb0cf2b9c250edcaf
Pau111111 <paugarciapellicer96@gmail.com>
2022-09-05 12:03:50 +0200

    Login demo in another tab

--- workshop/6-demo.php
+++ workshop/6-demo.php
@@ -2,6 +2,6 @@
     <div class="card-body">
         <h5 class="card-title">PHP Login with Sessions Demo</h5>
         <p class="card-text">Let's practice Sessions coding a Login example</p>
-        <a class="btn btn-primary" role="button" href="./workshop/demo/">Login Demo</a>
+        <a class="btn btn-primary" role="button" href="./workshop/demo/" target="_blank">Login Demo</a>
     </div>
 </div>
